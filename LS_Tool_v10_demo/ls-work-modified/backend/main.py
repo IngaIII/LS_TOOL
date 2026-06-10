aa46726fd1acf73e23fcc1a36ffabd8dd9b13f7e
IngaIII <ingakabane76@gmail.com>
2026-06-11 01:32:12 +0200

    Update main.py for port listening

--- LS_Tool_v10_demo/ls-work-modified/backend/main.py
+++ LS_Tool_v10_demo/ls-work-modified/backend/main.py
@@ -122,4 +122,4 @@ if os.path.exists(FRONTEND_DIST):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
